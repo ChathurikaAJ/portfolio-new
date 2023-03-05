e1c1503891fb6e9924df200b30e014bd6da2e6db
Chathurika <chathurika-jayakody.com>
2023-03-04 20:41:03 -0500

    Add header

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import logo from './assets/logo/logo.png'
+import { Canvas } from '@react-three/fiber';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <header className="App__header">
+      <img src={logo} alt="logo" className="App__logo"/>
+    </header>
+
+    {/* <Canvas shadows> */}
+      <App />
+    {/* </Canvas> */}
+
   </React.StrictMode>
 );
 
