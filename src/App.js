b9c5ff1be94465dfa983f3de31faf375f2cd8e80
Chathurika <chathurika-jayakody.com>
2023-03-05 21:28:36 -0500

    Add animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Environment, Scroll, ScrollControls, Sparkles, Float } from '@react-thr
 import project1 from './assets/images/pronounce.PNG'
 import project2 from './assets/images/colombotodo.PNG'
 import { useState } from 'react';
+import {Lotus} from './models/Lotus'
+import {Bird} from './models/Bird'
+
 
 function App() {
 
@@ -23,6 +26,19 @@ function App() {
       <ScrollControls pages={3} damping={0.25}>
 
         <Scroll>
+
+        <Float
+          speed={1} // Animation speed, defaults to 1
+          rotationIntensity={1} // XYZ rotation intensity, defaults to 1
+          floatIntensity={0.2} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+          floatingRange={[1, 1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        >
+          <Bird scale={2} position={[0,-2.5,0]}/>
+       
+        </Float>
+
+
+
           <Sparkles noise={0} count={500} speed={0.01} size={0.6} color={"#FFD2BE"} opacity={10} scale={[20, 100, 20]}></Sparkles>
           <Sparkles noise={0} count={50} speed={0.01} size={10} color={"#FFF"} opacity={2} scale={[30, 100, 10]} ></Sparkles>
         </Scroll>
