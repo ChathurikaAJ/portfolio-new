ab5c328e1b144191627ce7e747229ef6a755fc54
Chathurika <chathurika-jayakody.com>
2023-03-09 18:06:59 -0500

    Update contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
-import { BsGithub, BsLinkedin} from 'react-icons/bs';
-import {SiGmail} from 'react-icons/si';
-import {Row, Col } from 'react-bootstrap';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { SiGmail } from 'react-icons/si';
+import { Row, Col } from 'react-bootstrap';
 
 const Contact = () => {
     return (
@@ -11,9 +11,15 @@ const Contact = () => {
                 </div>
 
                 <div className='App__icons'>
-                    <BsGithub color='white' size='60px' cursor='pointer' />
-                    <BsLinkedin color='white' size='60px' cursor='pointer' />
-                    <SiGmail color='white' size='60px' cursor='pointer' />
+                    <a href="https://github.com/ChathurikaAJ" target="_blank" rel="noreferrer">
+                        <BsGithub color='white' size='60px' cursor='pointer' />
+                    </a>
+                    <a href="https://www.linkedin.com/in/chathurika-jayakody" target="_blank" rel="noreferrer">
+                        <BsLinkedin color='white' size='60px' cursor='pointer' />
+                    </a>
+                    <a href="mailto:chathurika.jayakody@gmail.com">
+                        <SiGmail color='white' size='60px' cursor='pointer' />
+                    </a>
 
                 </div>
             </Col>
